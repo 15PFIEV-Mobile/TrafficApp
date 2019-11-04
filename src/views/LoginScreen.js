18fe67c1427d4fcc2fbee68e2c1d32da1b551549
QuocBao <baoquocho1997@gmail.com>
2019-11-04 15:21:58 +0700

    re-create welcome login signup UI

--- src/views/LoginScreen.js
+++ src/views/LoginScreen.js
@@ -1,62 +1,233 @@
-import React, { Component } from "react";
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import Button from "react-native-button";
+import { AppStyles } from "../AppStyles";
+import firebase from "react-native-firebase";
+import { AsyncStorage } from "react-native";
+const FBSDK = require("react-native-fbsdk");
+const { LoginManager, AccessToken } = FBSDK;
 
-import styles from "./styles/login.style";
-import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView} from 'react-native';
-import { Button } from 'react-native-elements';
+class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      email: "",
+      password: ""
+    };
+  }
 
-const appId = "1047121222092614"
+  onPressLogin = () => {
+    const { email, password } = this.state;
+    if (email.length <= 0 || password.length <= 0) {
+      alert("Please fill out the required fields.");
+      return;
+    }
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        const { navigation } = this.props;
+        user_uid = response.user._user.uid;
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(user_uid)
+          .get()
+          .then(function(user) {
+            if (user.exists) {
+              AsyncStorage.setItem("@loggedInUserID:id", user_uid);
+              AsyncStorage.setItem("@loggedInUserID:key", email);
+              AsyncStorage.setItem("@loggedInUserID:password", password);
+              navigation.dispatch({ type: "Login", user: user });
+            } else {
+              alert("User does not exist. Please try again.");
+            }
+          })
+          .catch(function(error) {
+            const { code, message } = error;
+            alert(message);
+          });
+      })
+      .catch(error => {
+        const { code, message } = error;
+        alert(message);
+        // For details of error codes, see the docs
+        // The message contains the default Firebase string
+        // representation of the error
+      });
+  };
 
-export default class LoginScreen extends Component {
+  onPressFacebook = () => {
+    LoginManager.logInWithReadPermissions([
+      "public_profile",
+      "user_friends",
+      "email"
+    ]).then(
+      result => {
+        if (result.isCancelled) {
+          alert("Whoops!", "You cancelled the sign in.");
+        } else {
+          AccessToken.getCurrentAccessToken().then(data => {
+            const credential = firebase.auth.FacebookAuthProvider.credential(
+              data.accessToken
+            );
+            const accessToken = data.accessToken;
+            firebase
+              .auth()
+              .signInWithCredential(credential)
+              .then(result => {
+                var user = result.user;
+                AsyncStorage.setItem(
+                  "@loggedInUserID:facebookCredentialAccessToken",
+                  accessToken
+                );
+                AsyncStorage.setItem("@loggedInUserID:id", user.uid);
+                var userDict = {
+                  id: user.uid,
+                  fullname: user.displayName,
+                  email: user.email,
+                  profileURL: user.photoURL
+                };
+                var data = {
+                  ...userDict,
+                  appIdentifier: "rn-android-universal-listings"
+                };
+                firebase
+                  .firestore()
+                  .collection("users")
+                  .doc(user.uid)
+                  .set(data);
+                this.props.navigation.dispatch({
+                  type: "Login",
+                  user: userDict
+                });
+              })
+              .catch(error => {
+                alert("Please try again! " + error);
+              });
+          });
+        }
+      },
+      error => {
+        Alert.alert("Sign in error", error);
+      }
+    );
+  };
 
   render() {
     return (
-      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
-
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.loginScreenContainer}>
-          <View style={styles.loginFormView}>
-          <Text style={styles.logoText}>Instamobile</Text>
-            <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
-            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
-            <Button
-              buttonStyle={styles.loginButton}
-              onPress={() => this.onLoginPress()}
-              title="Login"
-            />
-            <Button
-              buttonStyle={styles.fbLoginButton}
-              onPress={() => this.onFbLoginPress()}
-              title="Login with Facebook"
-              color="#3897f1"
-            />
-          </View>
+      <View style={styles.container}>
+        <Text style={[styles.title, styles.leftTitle]}>Sign In</Text>
+        <View style={styles.InputContainer}>
+          <TextInput
+            style={styles.body}
+            placeholder="E-mail or phone number"
+            onChangeText={text => this.setState({ email: text })}
+            value={this.state.email}
+            placeholderTextColor={AppStyles.color.grey}
+            underlineColorAndroid="transparent"
+          />
         </View>
-      </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
+        <View style={styles.InputContainer}>
+          <TextInput
+            style={styles.body}
+            secureTextEntry={true}
+            placeholder="Password"
+            onChangeText={text => this.setState({ password: text })}
+            value={this.state.password}
+            placeholderTextColor={AppStyles.color.grey}
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <Button
+          containerStyle={styles.loginContainer}
+          style={styles.loginText}
+          onPress={() => this.onPressLogin()}
+        >
+          Log in
+        </Button>
+        <Text style={styles.or}>OR</Text>
+        <Button
+          containerStyle={styles.facebookContainer}
+          style={styles.facebookText}
+          onPress={() => this.onPressFacebook()}
+        >
+          Login with Facebook
+        </Button>
+      </View>
     );
   }
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  onLoginPress() {
-
-  }
-
-  async onFbLoginPress() {
-    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(appId, {
-      permissions: ['public_profile', 'email'],
-    });
-    if (type === 'success') {
-      const response = await fetch(
-        `https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert(
-        'Logged in!',
-        `Hi ${(await response.json()).name}!`,
-      );
-    }
-  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  or: {
+    fontFamily: AppStyles.fontName.main,
+    color: "black",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  title: {
+    fontSize: AppStyles.fontSize.title,
+    fontWeight: "bold",
+    color: AppStyles.color.tint,
+    marginTop: 20,
+    marginBottom: 20
+  },
+  leftTitle: {
+    alignSelf: "stretch",
+    textAlign: "left",
+    marginLeft: 20
+  },
+  content: {
+    paddingLeft: 50,
+    paddingRight: 50,
+    textAlign: "center",
+    fontSize: AppStyles.fontSize.content,
+    color: AppStyles.color.text
+  },
+  loginContainer: {
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.tint,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30
+  },
+  loginText: {
+    color: AppStyles.color.white
+  },
+  placeholder: {
+    fontFamily: AppStyles.fontName.text,
+    color: "red"
+  },
+  InputContainer: {
+    width: AppStyles.textInputWidth.main,
+    marginTop: 30,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: AppStyles.color.grey,
+    borderRadius: AppStyles.borderRadius.main
+  },
+  body: {
+    height: 42,
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: AppStyles.color.text
+  },
+  facebookContainer: {
+    width: AppStyles.buttonWidth.main,
+    backgroundColor: AppStyles.color.facebook,
+    borderRadius: AppStyles.borderRadius.main,
+    padding: 10,
+    marginTop: 30
+  },
+  facebookText: {
+    color: AppStyles.color.white
+  }
+});
+
+export default LoginScreen;
