30a76580e8ca5fd6232d3d5d90e41dec53cf8b76
Chinh Tran <chinhpfiev@gmail.com>
2019-11-11 00:35:35 +0700

    Load screen

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Button, Icon, Title, Segment, Content, Text } from 'native-base';
 export default class HomeScreen extends Component {
+  state = {
+    screen : 'maps'
+  }
+
+  changeScreen = (scr) => {
+    this.setState({
+      screen : scr
+    })
+  }
+
   render() {
+    screen = this.state.screen==='maps'?'Man hinh maps':'Man hinh streams'
     return (
       <Container>
         <Header hasSegment>
+          <Left>
+            <Button transparent>
+              <Icon type="FontAwesome"></Icon>
+            </Button>
+          </Left>
           <Body>
             <Title>Home</Title>
           </Body>
+          <Right>
+            <Button transparent>
+              
+            </Button>
+          </Right>
         </Header>
         <Segment>
-            <Button first active>
+            <Button first active={this.state.screen==='maps'} onPress={(event)=>this.changeScreen('maps')}>
                 <Text>Maps</Text>
             </Button>
-            <Button last>
+            <Button last active={this.state.screen==='streams'} onPress={(event)=>this.changeScreen('streams')}>
                 <Text>Streams</Text>
             </Button>
         </Segment>
         <Content padder>
-          <Text>Màn hình list stream</Text>
+          <Text>
+            {screen}
+          </Text>
         </Content>
       </Container>
     );
